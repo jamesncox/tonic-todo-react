c41956a4e138a492f478557230b7ff2aa8aefd7b
jamesncox <jamesncox@yahoo.com>
2021-05-04 12:08:59 -0500

    Update button colors and hover color

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,10 +2,10 @@ export default function Home(props) {
   return (
     <div className="flex-col text-center">
       <h1 className="text-matrix-green font-mono text-xl">Log in or sign up</h1>
-      <button className="bg-matrix-green text-black font-mono font-bold text-lg rounded-sm mx-4 mt-8 p-2 hover:bg-matrix-green-primary">
+      <button className="bg-matrix-green-primary text-black font-mono font-bold text-lg rounded-sm mx-4 mt-8 p-2 hover:bg-matrix-green-secondary">
         Log In
       </button>
-      <button className="bg-matrix-green text-black font-mono font-bold text-lg rounded-sm mx-4 mt-8 p-2 hover:bg-matrix-green-primary">
+      <button className="bg-matrix-green-primary text-black font-mono font-bold text-lg rounded-sm mx-4 mt-8 p-2 hover:bg-matrix-green-secondary">
         Sign Up
       </button>
     </div>
