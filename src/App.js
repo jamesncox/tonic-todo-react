1358f174568e858d87fb13e73d104155ca79a8df
jamesncox <jamesncox@yahoo.com>
2021-05-04 11:20:24 -0500

    Create Home.js and add to switch inside App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { useEffect } from "react";
-
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { connect } from "react-redux";
 import { getToken } from "./actions/sessions";
 import { setCurrentUser } from "./actions/users";
 import "./stylesheets/App.css";
+
+import Home from "./pages/Home";
 
 function App(props) {
   useEffect(() => {
@@ -15,9 +16,14 @@ function App(props) {
   return (
     <Router>
       <div className="bg-black h-screen pt-4 pl-4">
-        <header className="text-matrix-green text-4xl font-bold font-mono">
-          Wake up, Neo...
+        <header className="text-matrix-green text-2xl md:text-4xl font-bold font-mono">
+          Wake up, Tonic...
         </header>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
       </div>
     </Router>
   );
---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+export default function Home(props) {
+  return (
+    <div className="flex-col justify-center">
+      <h1 className="text-matrix-green font-mono">HEY THERE</h1>
+      <button className="bg-matrix-green text-white">Log In</button>
+      <button>Sign Up</button>
+    </div>
+  );
+}
