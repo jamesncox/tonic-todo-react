2c5b65440e5325472041c4e38db633265a1202f5
jamesncox <jamesncox@yahoo.com>
2021-05-04 13:57:18 -0500

    Update Home.js with Link

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,23 @@
+import { Link } from "react-router-dom";
+
 export default function Home(props) {
   return (
     <div className="flex-col text-center">
-      <h1 className="text-matrix-green font-mono text-xl">Log in or sign up</h1>
-      <button className="bg-matrix-green-primary text-black font-mono font-bold text-lg rounded-sm mx-4 mt-8 p-2 hover:bg-matrix-green-hover">
+      <h1 className="text-matrix-green font-mono font-extrabold text-xl mb-8">
+        Log in or sign up
+      </h1>
+      <Link
+        to="/login"
+        className="bg-matrix-green-primary text-black font-mono font-bold text-lg rounded-sm mx-4 mt-8 p-2 hover:bg-matrix-green-hover"
+      >
         Log In
-      </button>
-      <button className="bg-matrix-green-primary text-black font-mono font-bold text-lg rounded-sm mx-4 mt-8 p-2 hover:bg-matrix-green-hover">
+      </Link>
+      <Link
+        to="/signup"
+        className="bg-matrix-green-primary text-black font-mono font-bold text-lg rounded-sm mx-4 mt-8 p-2 hover:bg-matrix-green-hover"
+      >
         Sign Up
-      </button>
+      </Link>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
         "matrix-green": "#22b455",
         "matrix-green-primary": "#008F11",
         "matrix-green-secondary": "#003B00",
-        "matrix-green-hover": "#92e5a1",
+        "matrix-green-hover": "#00ff2b",
       },
       fontFamily: {
         mono: ["courier"],
