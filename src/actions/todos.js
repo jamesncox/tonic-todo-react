ec502e8bcbe28a0e5aa73fa5cda0ff3098bca343
jamesncox <jamesncox@yahoo.com>
2021-05-04 10:22:12 -0500

    Create actions/todos.js

---
+++ src/actions/todos.js
@@ -0,0 +1,105 @@
+import {
+  SET_TODOS,
+  LOADING_TODOS,
+  CREATE_TODO,
+  SET_ERRORS,
+  LOADING_SINGLE_TODO,
+  UPDATE_TODO,
+  DELETE_TODO,
+} from "../actionTypes";
+
+const setTodos = (todos) => {
+  return { type: SET_TODOS, payload: todos };
+};
+
+export const getTodos = (id) => {
+  return async (dispatch) => {
+    dispatch({ type: LOADING_TODOS });
+
+    try {
+      const res = await fetch(`https://localhost:3001/api/v1/user_todos/${id}`);
+      if (!res.ok) {
+        throw res;
+      }
+      const todoData = await res.json();
+      dispatch(setTodos(todoData));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+};
+
+export const createTodo = (todo) => {
+  return async (dispatch) => {
+    dispatch({ type: LOADING_SINGLE_TODO });
+
+    const formData = {
+      title: todo.title,
+      done: todo.done,
+      user_id: todo.userId,
+    };
+
+    const res = await fetch("https://localhost:3001/api/v1/todos", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(formData),
+      credentials: "include",
+    });
+
+    const todoObj = await res.json();
+
+    if (todoObj.errors) {
+      dispatch({ type: SET_ERRORS, payload: todoObj.errors });
+    } else {
+      dispatch({ type: CREATE_TODO, payload: todoObj });
+    }
+  };
+};
+
+export const changeTodoStatus = (todo) => {
+  return async (dispatch) => {
+    dispatch({ type: LOADING_TODOS });
+
+    const formData = {
+      title: todo.title,
+      done: todo.done,
+      user_id: todo.userId,
+    };
+
+    const res = await fetch(
+      `https://localhost:3001/api/v1/todos/${todo.todoId}`,
+      {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify(formData),
+        credentials: "include",
+      }
+    );
+
+    const todoObj = await res.json();
+
+    if (todoObj.errors) {
+      dispatch({ type: SET_ERRORS, payload: todoObj.errors });
+    } else {
+      dispatch({ type: UPDATE_TODO, payload: todoObj });
+    }
+  };
+};
+
+export function deleteTodo(id) {
+  return (dispatch) => {
+    dispatch({ type: LOADING_TODOS });
+
+    fetch(`https://localhost:3001/api/v1/todos/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((todo) => dispatch({ type: DELETE_TODO, payload: todo }));
+  };
+}
