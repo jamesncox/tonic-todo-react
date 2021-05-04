d95b6a5007bc079b5173f97da9a79fba113a34b9
jamesncox <jamesncox@yahoo.com>
2021-05-04 10:15:59 -0500

    Create sessions.js/add react-router-dom

---
+++ src/actions/sessions.js
@@ -0,0 +1,15 @@
+import { SET_TOKEN } from "../actionTypes";
+
+const setToken = (token) => {
+  return (dispatch) => {
+    dispatch({ type: SET_TOKEN, payload: token });
+  };
+};
+
+export function getToken() {
+  return (dispatch) => {
+    fetch("https://localhost:3001/api/v1/auth_check")
+      .then((res) => res.json())
+      .then((token) => dispatch(setToken(token.csrf_auth_token)));
+  };
+}
