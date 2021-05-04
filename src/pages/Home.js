6d07c3cf9f158df948fc92f4da1bae7e72fc16f4
jamesncox <jamesncox@yahoo.com>
2021-05-04 13:20:37 -0500

    Update button hover

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,10 +2,10 @@ export default function Home(props) {
   return (
     <div className="flex-col text-center">
       <h1 className="text-matrix-green font-mono text-xl">Log in or sign up</h1>
-      <button className="bg-matrix-green-primary text-black font-mono font-bold text-lg rounded-sm mx-4 mt-8 p-2 hover:bg-matrix-green-secondary">
+      <button className="bg-matrix-green-primary text-black font-mono font-bold text-lg rounded-sm mx-4 mt-8 p-2 hover:bg-matrix-green-hover">
         Log In
       </button>
-      <button className="bg-matrix-green-primary text-black font-mono font-bold text-lg rounded-sm mx-4 mt-8 p-2 hover:bg-matrix-green-secondary">
+      <button className="bg-matrix-green-primary text-black font-mono font-bold text-lg rounded-sm mx-4 mt-8 p-2 hover:bg-matrix-green-hover">
         Sign Up
       </button>
     </div>
