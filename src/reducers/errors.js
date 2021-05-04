9731d1e27fbebf16064e5e7551504f6eda08bd8e
jamesncox <jamesncox@yahoo.com>
2021-05-04 10:24:48 -0500

    Create reducers/errors.js

---
+++ src/reducers/errors.js
@@ -0,0 +1,19 @@
+import { SET_ERRORS, CLEAR_ERRORS } from "../actionTypes";
+
+export default (
+  state = {
+    errors: null,
+  },
+  action
+) => {
+  switch (action.type) {
+    case SET_ERRORS:
+      return { ...state, errors: action.payload };
+
+    case CLEAR_ERRORS:
+      return { ...state, errors: null };
+
+    default:
+      return state;
+  }
+};
