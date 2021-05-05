c79ac6bd1e77da436932529bfb0cd979facedc68
jamesncox <jamesncox@yahoo.com>
2021-05-05 17:55:00 -0500

    Remove {getTodos} from imports

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import Footer from "./components/Footer";
 import Todos from "./pages/Todos";
-import { getTodos } from "./actions/todos";
 
 function App({ getToken, setCurrentUser }) {
   useEffect(() => {
