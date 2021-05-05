76bdef8164df6c910a5a4a28a9f27fad0b47d90a
jamesncox <jamesncox@yahoo.com>
2021-05-05 12:30:03 -0500

    Style Loader

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { Redirect } from "react-router";
+import Loader from "../components/Loader";
 
 function Home(props) {
   if (props.loggedIn) {
@@ -23,6 +24,7 @@ function Home(props) {
         >
           Sign Up
         </Link>
+        <Loader />
       </div>
     );
   }
