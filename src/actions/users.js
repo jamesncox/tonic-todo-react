1a5496ae8662b7a1e0954ae4618b588d3f491a32
jamesncox <jamesncox@yahoo.com>
2021-05-04 22:50:52 -0500

    Remove alert from users

--- src/actions/users.js
+++ src/actions/users.js
@@ -97,7 +97,7 @@ export function setCurrentUser() {
       const userObj = await res.json();
       dispatch({ type: SET_USER, payload: userObj });
     } catch (err) {
-      alert(err.message);
+      // alert(err.message);
     }
   };
 }
