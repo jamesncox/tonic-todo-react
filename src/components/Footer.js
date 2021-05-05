797c7d428b8bd6aadda5dddc427b7052c4c75d80
jamesncox <jamesncox@yahoo.com>
2021-05-04 23:25:14 -0500

    Update Footer widths and breakpoints

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <div className="text-center border-t border-matrix-green mx-5 pt-5 mt-auto">
+    <div className="text-center border-t border-matrix-green mx-5 pt-5 mt-auto xl:w-1/2 xl:mx-auto">
       <p className="text-matrix-green">
         Made with ❤️‍ by{" "}
         <a
