8c0b1c80fc7ea61e9ee42b19777bc515345403eb
jamesncox <jamesncox@yahoo.com>
2021-05-06 09:44:04 -0500

    Update Loader image styles and breakpoint height

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -6,18 +6,18 @@ export default function Loader() {
       <div className="mx-5 justify-center mt-32 flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
         <div className="relative w-auto my-6 mx-auto max-w-3xl">
           <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-            <div className="h-64 bg-matrix-green-text">
+            <div className="h-44 md:h-64 bg-matrix-green-text rounded rounded-b-none">
               <img
                 src={MatrixScroll}
                 alt="Gif of The Matrix code scrolling effect"
               />
             </div>
-            <div className="flex justify-center p-2 bg-matrix-green-hover rounded rounded-b-none">
+            <div className="flex justify-center p-2 bg-matrix-green-hover">
               <h3 className="text-2xl text-black font-mono font-bold">
                 COMPILING...
               </h3>
             </div>
-            <div className="h-64 bg-matrix-green-text">
+            <div className="h-44 md:h-64 bg-matrix-green-text rounded rounded-t-none">
               <img
                 src={MatrixScroll}
                 alt="Gif of The Matrix code scrolling effect"
