0b68e1b291ececb456f2ff57ac0da6ae49f4e86d
jamesncox <jamesncox@yahoo.com>
2021-05-05 22:15:52 -0500

    Update Loader.js and Todos.js width breakpoints

--- src/pages/Todos.js
+++ src/pages/Todos.js
@@ -18,7 +18,7 @@ function Todos({ id, loggedIn, todos }) {
     return <Redirect to="/" />;
   } else {
     return (
-      <div className="sm:2/3 md:w-2/3 lg:w-1/2">
+      <div className="w-full sm:2/3 lg:w-1/2 xl:w-1/3 min-h-screen mt-16">
         <AddTodo />
         <ul>
           {todos
