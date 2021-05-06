fedf98f6750821044f1473220d9bf72ee80a5db1
jamesncox <jamesncox@yahoo.com>
2021-05-06 09:04:53 -0500

    Update checkbox styles with matrix-greens

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -15,7 +15,7 @@ function TodoItem({ todo, deleteTodo, changeTodoStatus }) {
     <div className="flex justify-between items-center mx-5 md:mx-0 mt-5 bg-matrix-green p-4 rounded last:mb-5">
       <div className="flex items-center">
         <input
-          className="mr-5 form-checkbox h-5 w-5 text-green-600"
+          className="mr-5 form-checkbox h-5 w-5 text-matrix-green-text bg-matrix-green-light rounded"
           type="checkbox"
           id={todo.id}
           name={todo}
