95b25d2a226bb519d10dca0a77d8fd55cdb569d3
jamesncox <jamesncox@yahoo.com>
2021-05-07 11:22:01 -0500

    Update Loader/Error styles, margins

--- src/components/Errors.js
+++ src/components/Errors.js
@@ -13,8 +13,8 @@ function Errors({ errors, clearErrors, clearIsUserLoading }) {
 
   return (
     <>
-      <div className="mx-5 justify-center mt-32 flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-        <div className="relative w-auto my-6 mx-auto max-w-3xl">
+      <div className="mx-5 justify-center mt-36 flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+        <div className="relative w-auto my-6 mx-auto max-w-xl">
           <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
             <div className="flex justify-center p-5 bg-matrix-green rounded rounded-b-none">
               <h3 className="text-3xl text-matrix-green-secondary font-mono font-bold">
--- src/components/Loader.js
+++ src/components/Loader.js
@@ -3,22 +3,15 @@ import MatrixScroll from "../assets/matrix-scroll-code.webp";
 export default function Loader() {
   return (
     <>
-      <div className="mx-5 justify-center mt-32 flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+      <div className="mx-5 justify-center mt-40 flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
         <div className="relative w-auto my-6 mx-auto max-w-3xl">
-          <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-            <div className="h-44 md:h-64 bg-black rounded rounded-b-none">
-              <img
-                width="480"
-                src={MatrixScroll}
-                alt="Gif of The Matrix code scrolling effect"
-              />
-            </div>
-            <div className="flex justify-center p-2 bg-matrix-green-hover">
+          <div className="border-0 rounded shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none bg-matrix-green-hover p-5">
+            <div className="flex justify-center">
               <h3 className="text-2xl text-black font-mono font-bold">
                 COMPILING...
               </h3>
             </div>
-            <div className="h-44 md:h-64 bg-black rounded rounded-t-none">
+            <div className="h-44 md:h-64 bg-black">
               <img
                 width="480"
                 src={MatrixScroll}
