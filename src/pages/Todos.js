e894c35c8495cf9698964966dfe790f80fd8af2a
jamesncox <jamesncox@yahoo.com>
2021-05-20 08:38:25 -0500

    Update with conditional && render

--- src/pages/Todos.js
+++ src/pages/Todos.js
@@ -21,29 +21,25 @@ function Todos({ id, loggedIn, todos, getTodos }) {
     return (
       <div className="w-full sm:2/3 lg:w-1/2 mt-16">
         <AddTodo />
-        {incompleteTodos.length > 0 ? (
+        {incompleteTodos.length > 0 && (
           <p className="text-matrix-green font-mono font-bold text-lg mx-5">
             Incomplete
           </p>
-        ) : null}
+        )}
         <ul>
-          {incompleteTodos
-            ? incompleteTodos.map((todo) => (
-                <TodoItem key={todo.id} todo={todo} />
-              ))
-            : null}
+          {incompleteTodos &&
+            incompleteTodos.map((todo) => (
+              <TodoItem key={todo.id} todo={todo} />
+            ))}
         </ul>
-        {completeTodos.length > 0 ? (
+        {completeTodos.length > 0 && (
           <p className="text-matrix-green font-mono font-bold text-lg mx-5">
             Complete
           </p>
-        ) : null}
+        )}
         <ul>
-          {completeTodos
-            ? completeTodos.map((todo) => (
-                <TodoItem key={todo.id} todo={todo} />
-              ))
-            : null}
+          {completeTodos &&
+            completeTodos.map((todo) => <TodoItem key={todo.id} todo={todo} />)}
         </ul>
       </div>
     );
